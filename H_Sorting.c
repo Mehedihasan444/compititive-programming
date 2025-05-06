7e5ef7d248a6dda03957c7800cae4f5d0bf7f8af
Mehedi <mehedihasan67705251@gmail.com>
2025-05-06 16:29:44 +0600

    Fix sorting algorithm loop boundaries in H_Sorting.c

--- H_Sorting.c
+++ H_Sorting.c
@@ -9,9 +9,9 @@ int main()
         scanf("%d", &arr[i]);
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n-1; i++)
     {
-        for (int j = i + 1; j <= n; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (arr[i] > arr[j])
             {
