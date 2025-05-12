48add0bab827b094af0dbe7ec9db74a31a19dfb7
Mehedi <mehedihasan67705251@gmail.com>
2025-05-12 23:29:30 +0600

    Add T_Matrix.c to calculate the absolute difference between the sums of the primary and secondary diagonals of a user-defined matrix

---
+++ T_Matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int p_sum = 0;
+    int s_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                p_sum += a[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i + j == n - 1)
+            {
+                s_sum += a[i][j];
+            }
+        }
+    }
+    printf("%d\n", abs(p_sum - s_sum));
+    return 0;
+}
