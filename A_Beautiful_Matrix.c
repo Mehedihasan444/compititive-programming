b389848f2c7073547c57bf56db1df5677174ead1
Mehedi <mehedihasan67705251@gmail.com>
2025-05-15 02:44:51 +0600

    Add A_Beautiful_Matrix.c to implement a program that reads a matrix and calculates distances from the center

---
+++ A_Beautiful_Matrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int arr[6][6];
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (arr[i][j] != 0)
+            {
+               
+                printf("%d\n", abs(abs(3 - i) + abs(3 - j)));
+            }
+        }
+    }
+
+    return 0;
+}
+
+/**
+ *
+ *  3*3 2*5
+ * 3*3 3*2
+ * **/
