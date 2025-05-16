6fc23dff72e0ae27f2ba1f14818c9b20ac926483
Mehedi <mehedihasan67705251@gmail.com>
2025-05-16 11:46:01 +0600

    Add Matrix_Again.c to implement a program that reads a matrix and prints the last row and last column

---
+++ Matrix_Again.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int m, n;
+    scanf("%d %d", &n, &m);
+    int a[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", a[n - 1][i]);
+    }
+    printf("\n");
+    for (int j = 0; j < n; j++)
+    {
+        printf("%d ", a[j][m - 1]);
+    }
+    return 0;
+}
