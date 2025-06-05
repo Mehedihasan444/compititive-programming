572ef6ee3b3386e3a5b90f4b0848f5567609df18
Mehedi <mehedihasan67705251@gmail.com>
2025-06-05 13:36:37 +0600

    feat: add V_Replace_Word.cpp to implement a program that replaces occurrences of 'EGYPT' with 'aaaa ' in a string

---
+++ V_Replace_Word.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'E' && s[i + 1] == 'G' && s[i + 2] == 'Y' && s[i + 3] == 'P' && s[i + 4] == 'T')
+        {
+            s[i] = 'a';
+            s[i + 1] = 'a';
+            s[i + 2] = 'a';
+            s[i + 3] = 'a';
+            s[i + 4] = ' ';
+           
+        }
+    }
+     for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i]!='a')
+        {
+            
+            cout << s[i];
+        }
+        
+    }
+    cout << endl;
+    return 0;
+}
