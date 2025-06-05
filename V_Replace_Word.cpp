dbf7e5f01ccc4bce36b066e54d008f9dbcb78c74
Mehedi <mehedihasan67705251@gmail.com>
2025-06-05 14:57:43 +0600

    refactor: simplify string replacement logic in V_Replace_Word.cpp

--- V_Replace_Word.cpp
+++ V_Replace_Word.cpp
@@ -9,23 +9,23 @@ int main()
     {
         if (s[i] == 'E' && s[i + 1] == 'G' && s[i + 2] == 'Y' && s[i + 3] == 'P' && s[i + 4] == 'T')
         {
-            s[i] = 'a';
-            s[i + 1] = 'a';
-            s[i + 2] = 'a';
-            s[i + 3] = 'a';
-            s[i + 4] = ' ';
-           
+            // s[i] = 'a';
+            // s[i + 1] = 'a';
+            // s[i + 2] = 'a';
+            // s[i + 3] = 'a';
+            // s[i + 4] = ' ';
+            s.replace(i, 5, " ");
         }
     }
-     for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i]!='a')
-        {
-            
-            cout << s[i];
-        }
-        
-    }
-    cout << endl;
+    //  for (int i = 0; i < s.size(); i++)
+    // {
+    //     if (s[i]!='a')
+    //     {
+
+    //         cout << s[i];
+    //     }
+
+    // }
+    cout << s << endl;
     return 0;
 }
