d986615c18f45592ac8650533ea52078fafb8008
Mehedi <mehedihasan67705251@gmail.com>
2025-06-13 00:09:20 +0600

    feat: add Find_Jessica.cpp to implement a program that checks for the presence of the word "Jessica" in a given input string

---
+++ Find_Jessica.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    getline(cin , s);
+    stringstream ss(s);
+    string word;
+    int f = 0;
+    while (ss >> word)
+    {
+        if (word == "Jessica")
+        {
+            cout << "YES" << endl;
+            f = 1;
+            break;
+        }
+    }
+    if (f == 0)
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
