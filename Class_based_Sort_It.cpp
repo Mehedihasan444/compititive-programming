263c452443eaae1d02ddc730e07fcf278db90f3d
Mehedi <mehedihasan67705251@gmail.com>
2025-06-13 01:01:56 +0600

    feat: add Class_based_Sort_It.cpp to implement a program that sorts student records based on the sum of their math and English marks

---
+++ Class_based_Sort_It.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string nm;
+    string cls;
+    char s;
+    int id;
+    int math_marks;
+    int eng_marks;
+};
+bool cmp(Student l, Student r)
+{
+    int l_marks = l.math_marks + l.eng_marks;
+    int r_marks = r.math_marks + r.eng_marks;
+    return l_marks > r_marks ? true : l_marks == r_marks ? l.id < r.id ? true : false
+                                                         : false;
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    Student students[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> students[i].nm >> students[i].cls >>
+            students[i].s >> students[i].id >> students[i].math_marks >> students[i].eng_marks;
+    }
+
+    sort(students, students + n, cmp);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << students[i].nm << " " << students[i].cls << " " << students[i].s << " " << students[i].id << " "<< students[i].math_marks << " "<< students[i].eng_marks<<endl;
+    }
+
+    return 0;
+}
