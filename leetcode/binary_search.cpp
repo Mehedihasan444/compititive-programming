a9dd058b72855cd97c94e3ec0cd8291cce72b209
Mehedi <mehedihasan67705251@gmail.com>
2025-06-23 15:58:22 +0600

    feat: add binary_search.cpp to implement a binary search algorithm for sorted arrays

---
+++ leetcode/binary_search.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+    if (nums.empty())
+    {
+        return -1;
+    }
+    int len = nums.size();
+
+    int l = 0, r = len - 1;
+
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] < target)
+        {
+            l = mid + 1;
+        }
+        else if (nums[mid] > target)
+        {
+            r = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int x;
+    cin >> x;
+    int r = search(arr, x);
+    cout << r << endl;
+    return 0;
+}
