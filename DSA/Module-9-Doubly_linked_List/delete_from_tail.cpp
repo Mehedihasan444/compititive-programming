1b4b8600ba93591b85eb69457e185f08770f1ec2
Mehedi <mehedihasan67705251@gmail.com>
2025-07-08 19:03:07 +0600

    feat: add delete_from_tail.cpp to implement deletion from the tail of a doubly linked list

---
+++ DSA/Module-9-Doubly_linked_List/delete_from_tail.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *prev;
+    Node *next;
+    int val;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    newNode->prev = tail;
+    tail->next = newNode;
+    tail = newNode;
+}
+void delete_from_tail(Node *&head, Node *&tail)
+{
+    Node *deleteNode = tail;
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (tail->prev == NULL)
+    {
+        tail = NULL;
+        head = NULL;
+        delete deleteNode;
+        return;
+    }
+    tail = tail->prev;
+    tail->next = NULL;
+    delete deleteNode;
+}
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL, *tail = NULL;
+    while (1)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    delete_from_tail(head, tail);
+    print_linked_list(head);
+
+    return 0;
+}
