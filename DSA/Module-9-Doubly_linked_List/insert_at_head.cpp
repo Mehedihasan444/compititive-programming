fed46037667823e51907f2457019bc8cdfafce57
Mehedi <mehedihasan67705251@gmail.com>
2025-07-08 17:59:19 +0600

    feat: add insert_at_head.cpp to implement insertion at the head of a doubly linked list

---
+++ DSA/Module-9-Doubly_linked_List/insert_at_head.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *prev;
+    Node *next;
+    int val;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    newNode->prev = tail;
+    tail->next = newNode;
+    tail = newNode;
+}
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    head->prev = newNode;
+    head = newNode;
+}
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL, *tail = NULL;
+    while (1)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    insert_at_head(head, tail, 11);
+    print_linked_list(head);
+
+    return 0;
+}
