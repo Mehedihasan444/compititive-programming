dd2d04263adffebcadbafb48f2ed474c27689ac6
Mehedi <mehedihasan67705251@gmail.com>
2025-07-10 21:35:06 +0600

    feat: add contains-duplicate.cpp to implement a function that checks for duplicate elements in an array

---
+++ leetcode/contains-duplicate.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool containsDuplicate(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    bool result = containsDuplicate(nums);
+    cout << (result==1?"true":"false") << endl;
+    return 0;
+}
