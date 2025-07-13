07b3eed9d3527def6233ce14e49b082af06d0fce
Mehedi <mehedihasan67705251@gmail.com>
2025-07-13 23:07:34 +0600

    feat: add linked-list-cycle.cpp to implement a function that detects a cycle in a linked list

---
+++ leetcode/linked-list-cycle.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    ListNode *fast = head, *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    ListNode *head = NULL, *tail = NULL;
+
+    while (1)
+    {
+        int x;
+        cin >> x;
+        if (x == -1)
+            break;
+
+        ListNode *newNode = new ListNode(x);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    bool result = hasCycle(head);
+    cout << (result ? "true" : "false") << endl;
+
+    return 0;
+}
