a8279774d6cbb5e41f83ef8e4ff99047d5b5f5cf
Mehedi <mehedihasan67705251@gmail.com>
2025-07-20 23:59:53 +0600

    feat: add implement_qqueue_using_singly_linked_list.cpp to implement a queue data structure using a singly linked list

---
+++ DSA/Module-14/implement_qqueue_using_singly_linked_list.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+public:
+    Node *head = nullptr;
+    Node *tail = nullptr;
+    int sz = 0;
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+
+        sz++;
+    }
+
+    void pop()
+    {
+        if (head == nullptr)
+            return;
+        Node *deleteNode = head;
+        head = head->next;
+        delete deleteNode;
+        sz--;
+        if (head == nullptr)
+            tail = nullptr;
+    }
+
+    int front()
+    {
+
+        return head->val;
+    }
+    int back()
+    {
+
+        return tail->val;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        return head == nullptr;
+    }
+};
+int main()
+{
+    myQueue q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
