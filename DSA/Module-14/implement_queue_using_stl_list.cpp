7f008417f3f61ef7435d433f26c74608202d578c
Mehedi <mehedihasan67705251@gmail.com>
2025-07-21 00:11:33 +0600

    feat: add implement_queue_using_stl_list.cpp to implement a queue data structure using the STL list

---
+++ DSA/Module-14/implement_queue_using_stl_list.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myQueue
+{
+public:
+    list<int> q;
+    void push(int val)
+    {
+        q.push_back(val);
+    }
+
+    void pop()
+    {
+        q.pop_front();
+    }
+
+    int front()
+    {
+
+        return q.front();
+    }
+    int back()
+    {
+
+        return q.back();
+    }
+    int size()
+    {
+        return q.size();
+    }
+    bool empty()
+    {
+        return q.empty();
+    }
+};
+int main()
+{
+    myQueue q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
