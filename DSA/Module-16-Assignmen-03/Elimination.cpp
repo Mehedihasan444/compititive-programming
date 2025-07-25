c1a8fe3e6d12d7f62a035d1305deef31e3cc5ed8
Mehedi <mehedihasan67705251@gmail.com>
2025-07-25 16:11:57 +0600

    feat: add Elimination.cpp to implement a program that eliminates pairs of '0' and '1' using a stack

---
+++ DSA/Module-16-Assignmen-03/Elimination.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        stack<char> stk;
+        string s;
+        cin >> s;
+        int len = s.size();
+        for (int i = 0; i < len; i++)
+        {
+            if (stk.empty())
+            {
+
+                stk.push(s[i]);
+            }
+            else
+            {
+                if ((stk.top() == '0' && s[i] == '1') )
+                {
+                    stk.pop();
+                }
+                else
+                {
+                    stk.push(s[i]);
+                }
+            }
+        }
+        cout << (stk.empty() ? "YES" : "NO") << endl;
+    }
+
+    return 0;
+}
