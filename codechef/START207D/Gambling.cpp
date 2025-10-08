3668afa884b84c7072621d6777516305f6eb2348
Mehedi <mehedihasan67705251@gmail.com>
2025-10-08 20:34:47 +0600

    feat: add Gambling program to calculate the difference from 7

---
+++ codechef/START207D/Gambling.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a;
+    cin >> a;
+
+    cout << 7 - a << "\n";
+
+    return 0;
+}
