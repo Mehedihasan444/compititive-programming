e1e0353c7516771aee743a204e372296a5d85718
Mehedi <mehedihasan67705251@gmail.com>
2025-10-15 20:33:57 +0600

    feat: add program to calculate total cost based on input values

---
+++ codechef/Spring_Cleaning.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x, y;
+    cin >> x >> y;
+
+    int total = (x * 30) + (y * 60);
+    cout << total << "\n";
+
+    return 0;
+}
