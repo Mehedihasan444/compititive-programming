7828b58daf1c1c2ea17ce093984bd748dc9cadbe
Mehedi <mehedihasan67705251@gmail.com>
2025-11-12 20:38:16 +0600

    feat: add program to calculate basketball score based on player inputs

---
+++ codechef/Basketball_Score.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(NULL);
+   int x,y;
+    cin>>x>>y;
+    cout<<(x*3+y*2)<<"\n";
+    return 0;
+}
